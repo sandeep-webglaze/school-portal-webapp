a2e26e3da6d26b723457902acf5c06807c361183
Sandeep Kumar <sandeep.webglaze@gmail.com>
2026-09-23 15:22:09 +0530

    Icon fixed

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from "next";
+
 import { SITE_NAME, SITE_DESCRIPTION } from "@/constants";
 
 export default function manifest(): MetadataRoute.Manifest {
@@ -6,33 +7,46 @@ export default function manifest(): MetadataRoute.Manifest {
     name: SITE_NAME,
     short_name: SITE_NAME,
     description: SITE_DESCRIPTION,
+
     start_url: "/",
     display: "standalone",
-    background_color: "#fff",
-    theme_color: "#1e4fa3",
-    prefer_related_applications: false,
     orientation: "any",
+
+    background_color: "#ffffff",
+    theme_color: "#1e4fa3",
+
+    prefer_related_applications: false,
+
     icons: [
       {
-        src: "/favicon.ico",
+        src: "/favicon-16x16.png",
         sizes: "16x16",
-        type: "image/x-icon",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
       },
       {
         src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/android-chrome-256x256.png",
-        sizes: "256x256",
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/maskable_icon_x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
     ],
   };
